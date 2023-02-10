a69f5bb7751de5056d27d42411315c97e1434dde
Tobias Antonsen <tobias@tsant.no>
2023-02-10 14:05:20 +0100

    fix: added more object types

--- pyflake_client/models/enums/object_type.py
+++ pyflake_client/models/enums/object_type.py
@@ -5,8 +5,16 @@ from enum import Enum
 class ObjectType(str, Enum):
     """ObjectType"""
     TABLE = "TABLE"
-    VIEW = "VIEWS"
+    VIEW = "VIEW"
     MATVIEW = "MATVIEW"
+    ACCOUNT = "ACCOUNT"
+    STAGE = "STAGE"
+    FILE_FORMAT = "FILE_FORMAT"
+    STREAM = "STREAM"
+    PROCEDURE = "PROCEDURE"
+    FUNCTION = "FUNCTION"
+    SEQUENCE = "SEQUENCE"
+    TASK = "TASK"
 
     def __str__(self):
         match self:
@@ -16,6 +24,22 @@ class ObjectType(str, Enum):
                 return "VIEW"
             case ObjectType.MATVIEW:
                 return "MATERIALIZED VIEW"
+            case ObjectType.ACCOUNT:
+                return "ACCOUNT"
+            case ObjectType.STAGE:
+                return "STAGE"
+            case ObjectType.FILE_FORMAT:
+                return "FILE FORMAT"
+            case ObjectType.STREAM:
+                return "STREAM"
+            case ObjectType.PROCEDURE:
+                return "PROCEDURE"
+            case ObjectType.FUNCTION:
+                return "FUNCTION"
+            case ObjectType.SEQUENCE:
+                return "SEQUENCE"
+            case ObjectType.TASK:
+                return "TASK"
             case _:
                 pass  # __str__ doesn't accept default case as return
         return ""
@@ -33,3 +57,19 @@ class ObjectType(str, Enum):
                 return "VIEWS"
             case ObjectType.MATVIEW:
                 return "MATERIALIZED VIEWS"
+            case ObjectType.ACCOUNT:
+                return "ACCOUNTS"
+            case ObjectType.STAGE:
+                return "STAGES"
+            case ObjectType.FILE_FORMAT:
+                return "FILE FORMATS"
+            case ObjectType.STREAM:
+                return "STREAMS"
+            case ObjectType.PROCEDURE:
+                return "PROCEDURES"
+            case ObjectType.FUNCTION:
+                return "FUNCTIONS"
+            case ObjectType.SEQUENCE:
+                return "SEQUENCES"
+            case ObjectType.TASK:
+                return "TASKS"
