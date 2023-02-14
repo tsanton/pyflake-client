a4b6f860fb37ac76dd1f62abf4b66bb3a092353d
Tobias Antonsen <tobias@tsant.no>
2023-02-14 12:32:42 +0000

    ci: setup from requirements.txt

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import os
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="pyflake-client",
     version=os.getenv("PYFLAKE_SEMVER", "0.0.1"),
@@ -23,9 +26,7 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        "snowflake-connector-python==2.9.0"
-    ],
+    install_requires=required,
     packages=setuptools.find_packages(exclude=["*tests*"]),
     python_requires=">=3.8",
 )
