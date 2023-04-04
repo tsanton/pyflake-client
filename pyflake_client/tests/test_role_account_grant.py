64a621714b2357ef90434c08f6937edd744f812c
torbjomg <torbjorn.gulbrandsen@fremtind.no>
2023-04-04 17:19:53 +0000

    fix: update role_account_grant tests

--- pyflake_client/tests/test_role_account_grant.py
+++ pyflake_client/tests/test_role_account_grant.py
@@ -8,6 +8,7 @@ from pyflake_client.models.assets.grant import Grant as GrantAsset
 from pyflake_client.models.assets.grants.role_account_grant import RoleAccountGrant
 from pyflake_client.models.describables.grant import Grant as DescribableGrant
 from pyflake_client.models.entities.grant import Grant as EntitiesGrant
+from pyflake_client.models.describables.role import Role as DescribableRole
 
 
 def test_grant_role_account_privilege(
@@ -27,7 +28,10 @@ def test_grant_role_account_privilege(
         flake.register_asset(privilege, assets_queue)
 
         ### Act ###
-        grants = flake.describe_many(DescribableGrant(role), EntitiesGrant)
+        grants = flake.describe_many(
+            describable=DescribableGrant(principal=DescribableRole(name=role.name)),
+            entity=EntitiesGrant,
+        )
 
         ### Assert ###
         assert grants is not None
@@ -63,26 +67,23 @@ def test_grant_role_account_privileges(
         flake.register_asset(privilege, assets_queue)
 
         ### Act ###
-        grants = flake.describe_many(DescribableGrant(role), EntitiesGrant)
+        grants = flake.describe_many(
+            describable=DescribableGrant(principal=DescribableRole(name=role.name)),
+            entity=EntitiesGrant,
+        )
 
         ### Assert ###
-        assert False
-        # assert granted.role_name == role.name
-        # assert len(granted.grants) == 2
+        assert grants is not None
+        assert len(grants) == 2
+        priv_create_acc = next(g for g in grants if g.privilege == "CREATE ACCOUNT")
+        assert priv_create_acc is not None
+        assert priv_create_acc.granted_on == "ACCOUNT"
+        assert priv_create_acc.granted_by == "ACCOUNTADMIN"
 
-        # priv_create_acc = next(
-        #     (r for r in granted.grants if r.privilege == "CREATE ACCOUNT"), None
-        # )
-        # assert priv_create_acc is not None
-        # assert priv_create_acc.granted_on == "ACCOUNT"
-        # assert priv_create_acc.granted_by == "ACCOUNTADMIN"
-
-        # priv_create_usr = next(
-        #     (r for r in granted.grants if r.privilege == "CREATE USER"), None
-        # )
-        # assert priv_create_usr is not None
-        # assert priv_create_usr.granted_on == "ACCOUNT"
-        # assert priv_create_usr.granted_by == "USERADMIN"
+        priv_create_user = next(g for g in grants if g.privilege == "CREATE USER")
+        assert priv_create_user is not None
+        assert priv_create_user.granted_on == "ACCOUNT"
+        assert priv_create_user.granted_by == "USERADMIN"
     finally:
         ### Cleanup ###
         flake.delete_assets(assets_queue)
