92a52fb3bc998518231277e101aba5fb262fb41b
torbjomg <torbjorn.gulbrandsen@fremtind.no>
2023-04-17 10:56:36 +0000

    fix: warehouse create statement errors

--- pyflake_client/models/assets/warehouse.py
+++ pyflake_client/models/assets/warehouse.py
@@ -3,6 +3,8 @@
 from typing import Union
 
 from dataclasses import dataclass
+
+from pyflake_client.models.assets.role import Role as RoleAsset
 from pyflake_client.models.assets.snowflake_asset_interface import ISnowflakeAsset
 from pyflake_client.models.assets.snowflake_principal_interface import ISnowflakePrincipal
 from pyflake_client.models.enums.warehouse_size import WarehouseSize 
@@ -25,13 +27,11 @@ class Warehouse(ISnowflakeAsset):
         if self.owner is None:
             raise ValueError("Create statement not supported for owner-less warehouses")
 
-        if isinstance(self.owner, RoleAsset):
-            grantee_type = "ROLE"
-        else:
-            raise NotImplementedError("Ownership is not implementer for asset of type {self.owner.__class__}")
+        if not isinstance(self.owner, RoleAsset):
+            raise NotImplementedError("Ownership is not implemented for asset of type {self.owner.__class__}")
         
         return f"""CREATE OR REPLACE WAREHOUSE {self.warehouse_name} WITH WAREHOUSE_SIZE = '{self.size.value}' WAREHOUSE_TYPE = '{self.warehouse_type}' AUTO_RESUME = {self.auto_resume} AUTO_SUSPEND = {self.auto_suspend} INITIALLY_SUSPENDED = {self.init_suspend} COMMENT = '{self.comment}';
-                   GRANT OWNERSHIP ON WAREHOUSE {self.warehouse_name} to {self.owner.get_identifier()} REVOKE CURRENT GRANTS;"""
+                   GRANT OWNERSHIP ON WAREHOUSE {self.warehouse_name} to {self.owner.get_snowflake_type()} {self.owner.get_identifier()} REVOKE CURRENT GRANTS;"""
 
     def get_delete_statement(self):
         """get_delete_statement"""
