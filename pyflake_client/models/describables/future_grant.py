f1a2fc1bc62a339c1d61710f7d59934ca1b3b170
Tobias Seines Antonsen <tobias@tsant.no>
2023-08-17 14:24:55 +0000

    feat: SHOW FUTURE GRANTS TO DATABASE ROLE instead of procedure

--- pyflake_client/models/describables/future_grant.py
+++ pyflake_client/models/describables/future_grant.py
@@ -27,44 +27,12 @@ class FutureGrant(ISnowflakeDescribable):
             return f"SHOW FUTURE GRANTS TO ROLE {self.principal.name}".upper()
 
         elif isinstance(self.principal, DatabaseRoleDescribable):
-            query = """
-with show_grants_to_database_role as procedure(database_name varchar, database_role_name varchar)
-    returns variant not null
-    language python
-    runtime_version = '3.10'
-    packages = ('snowflake-snowpark-python')
-    handler = 'show_grants_to_database_role_py'
-as $$
-def show_grants_to_database_role_py(snowpark_session, database_name_py:str, database_role_name_py:str):
-    res = []
-    try:
-        for row in snowpark_session.sql(f'SHOW FUTURE GRANTS IN DATABASE {database_name_py.upper()}').to_local_iterator():
-            if row['grant_to'] == 'DATABASE_ROLE' and row['grantee_name'] == database_role_name_py:
-                    res.append(row.as_dict())
-        for schema_object in snowpark_session.sql(f'SHOW SCHEMAS IN DATABASE {database_name_py.upper()}').to_local_iterator():
-            schema_name:str = schema_object['name']
-            if schema_name not in('INFORMATION_SCHEMA', 'PUBLIC'):
-                query:str = f'SHOW FUTURE GRANTS IN SCHEMA {database_name_py}.{schema_name}'.upper()
-                for row in snowpark_session.sql(query).to_local_iterator():
-                    if row['grant_to'] == 'DATABASE_ROLE' and row['grantee_name'] == database_role_name_py:
-                        res.append(row.as_dict())
-    except:
-        pass
-    return res
-$$
-call show_grants_to_database_role('%(s1)s','%(s2)s');
-        """ % {
-                "s1": self.principal.db_name,
-                "s2": self.principal.name,
-            }
+            return f"SHOW FUTURE GRANTS TO DATABASE ROLE {self.principal.db_name}.{self.principal.name}".upper()
         else:
             raise NotImplementedError(f"Future Grant describe statement for {self.__class__} is not implemented")
-        return query
 
     def is_procedure(self) -> bool:
-        if self.principal.get_snowflake_type() == "ROLE":
-            return False
-        return True
+        return False
 
     @classmethod
     def get_deserializer(cls) -> Callable[[Dict[str, Any]], FutureGrantEntity]:
